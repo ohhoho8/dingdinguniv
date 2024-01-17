0c008d8c48739c6c6fd8ed53ea93636be54dfde2
haeryung <haeryung8@gmail.com>
2024-01-17 15:22:53 +0900

    add: svg images and docs

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import background_pc from "./assets/background_pc.svg"
 
 function App() {
   return (
     <div className="App">
       <h1>start</h1>
+      <img src={background_pc} alt={background_pc}/>
     </div>
   );
 }
