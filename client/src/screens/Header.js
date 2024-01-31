7917a408ef6dc59c23ecf59eaa395f091703ef28
haeryung <haeryung8@gmail.com>
2024-01-31 10:46:40 +0900

    update: Header (update version)

--- client/src/screens/Header.js
+++ client/src/screens/Header.js
@@ -2,34 +2,22 @@ import React, {useState, useContext} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-scroll';
 import {FaBars} from 'react-icons/fa';
-import EmptyBox from '../components/EmptyBox';
 import logo_dindinguniv from '../assets/logo_dingdinguniv.svg'
 import { LanguageContext } from '../contexts/Language';
+import EmptyBox from '../components/EmptyBox';
 
-const StyledButton = styled.button`
-  padding: 10px 20px;
-  font-family: 'GmarketSansMedium', sans-serif;
-  font-size: 14px;
-  color: #878787
-  cursor: pointer;
-  margin-top: 30px;
-  margin-bottom: 10px;
-  margin-left: auto;
-
-  &:focus {
-    outline: none;
-  }
-`
 const StyledHeader = styled.header`
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   position: relative;
+  margin-left: 288px;
 
   .menuToggleBtn{
     display: none;
   }
 
   @media screen and (max-width: 665px) {
+    margin-left: 25px;
     align-items: center;
     .menuToggleBtn {
       display: block;
@@ -38,33 +26,28 @@ const StyledHeader = styled.header`
     }
   }
 `
-const DingdingunivImage = styled.img`
-  margin-left: 46px;
-
-  @media screen and (min-width: 666px) {
-    margin-left: 115px;
-  }
-`;
 const Nav=styled.ul`
   @media screen and (max-width: 665px) {
     display: ${(props)=> (props.isToggleOpen ? "block" : "none")};
     width: 100%;
     position: fixed;
     top: 70px;
-    right: 53.33px;
+    right: 25px;
   }
 `
 const MenuList = styled.div`
   display: flex;
-  gap: 62px;
   align-items: flex-end;
   justify-content: flex-end;
-  
+  margin-left: 500px;
+  margin-right: 122px;
 
   @media screen and (max-width: 665px) {
     flex-direction: column;
     align-items: flex-end;
     margin-top: 70px;
+    margin-left: auto;
+    margin-right: 25px;
     gap:30px;
   }
 `
@@ -72,12 +55,41 @@ const Menu=styled.div`
   font-family: 'GmarketSansMedium', sans-serif;
   font-size: 14px;
   color: #878787;
+  margin-right: 62px;
+
+  @media screen and (max-width: 665px) {
+    margin-right: 0px;
+  }
 `
 const MenuLink = styled(Link)`
   cursor: pointer;
   text-decoration: none;
   color: inherit;
-`;
+`
+const Language=styled.div`
+  cursor: pointer;
+  font-family: 'GmarketSansMedium', sans-serif;
+  font-size: 14px;
+  color: #878787;
+`
+const LanguageBold=styled.div`
+  cursor: pointer;
+  font-family: 'GmarketSansBold', sans-serif;
+  font-size: 14px;
+  color: #878787;
+`
+const LanguageBox=styled.div`
+  display: flex;
+  position: fixed;
+  right: 292px;
+  gap: 15px;
+
+  @media screen and (max-width: 666px) {
+    position: fixed;
+    right: 72px;
+  }
+`
+;
 
 
 const Header = () => {
@@ -89,12 +101,12 @@ const Header = () => {
   }
 
   return (
-    <>
-        <StyledButton onClick={toggleLanguage}>언어 변경 (현재 언어:{language})</StyledButton>
+    <>  
+        <EmptyBox height={100} />
         <StyledHeader>
-          <DingdingunivImage src={logo_dindinguniv} alt={logo_dindinguniv} />
+          <img src={logo_dindinguniv} alt={logo_dindinguniv} />
           <FaBars className="menuToggleBtn" onClick={handleToggleOpen} />
-          <Nav isToggleOpen={isToggleOpen}>
+          <Nav isToggleOpen={isToggleOpen} onClick={handleToggleOpen}>
             <MenuList>
               <Menu>
                 <MenuLink to="ai" smooth={true} duration={500}>AI Solution</MenuLink>
@@ -107,6 +119,19 @@ const Header = () => {
               </Menu>
             </MenuList>
           </Nav>
+          {language === 'ko' ? (
+            <LanguageBox>
+              <LanguageBold>Kor</LanguageBold>
+              <Language>|</Language>
+              <Language onClick={toggleLanguage}>Eng</Language>
+            </LanguageBox>
+          ) : (
+            <LanguageBox>
+              <Language onClick={toggleLanguage}>Kor</Language>
+              <Language>|</Language>
+              <LanguageBold>Eng</LanguageBold>
+            </LanguageBox>
+          )}
         </StyledHeader>
     </>
   );
